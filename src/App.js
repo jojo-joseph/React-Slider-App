e3c6e01798e38329dede58047a4aaf7243e5960e
jojo-joseph <jojojoseph1996@gmail.com>
2019-08-30 13:10:03 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+//import { render } from "react-dom";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Carousel from "./Carousel";
+
+
+function App() {
+  return (
+    <div>
+      <Carousel /> 
+    </div>
+  );
+}
+
+export default App;
+
